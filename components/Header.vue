f83bb30064b94f63d919b6970aa1cb3ddff590d7
pouria <pouria.rahimy.290@gmail.com>
2022-10-20 12:44:32 -0700

    create structure date-picker

---
+++ components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header w-100">
+    I am header
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent'
+}
+</script>
+
+<style scoped>
+  .header{
+    background-image: url("./assets/img/back.jpg");
+    display: flex;
+    background-size: cover;
+    background-position: center;
+    height: 169px;
+  }
+</style>
---
+++ components/Home/Content.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Content.cue"
+}
+</script>
+
+<style scoped>
+
+</style>
